8f4d544a1708304c65c236ec9bff20e05aadc4fa
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-05 16:45:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+let move = document.getElementById('dodger');
+// dodger.style.bottom = "100px";
+dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.backgroundColor = "#000000";
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+   move.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
+  if (left < 360) {
+    move.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
