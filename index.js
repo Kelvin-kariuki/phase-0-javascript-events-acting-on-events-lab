bd05f1965e8a68a0586a7594422c5f3996017420
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-05 16:41:49 +0300

    commit tree acef3049b99b760191faf2a0a54776c613992d90

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+let move = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+   move.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
+  if (left < 360) {
+    move.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
